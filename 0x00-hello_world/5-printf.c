f868be6b70ac5c0c2370b3e84ab060f44b1d60d3
Komoverratti <komonagato@gmail.com>
2022-03-13 03:58:58 -0700

    4 to 6 commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("Size of a char: %lu byte(s)\n", sized (char));
-printf("Size of a char: %lu byte(s)\n", sizedof(char));
-printf("Size of an int: %lu byte(s)\n", sizedof(int));
-printf("Size of a long int: %lu byte(s)\n", sizedof(long int));
-printf("Size of a long long int: %lu byte(s)\n", sizedof(long long int));
-printf("Size of a float: %lu byte(s)\n", sizedof(float));
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
